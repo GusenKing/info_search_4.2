afb9b0c154f1be8d61d69c040dd3ee0a31b740eb
Emin <emin.sarux2004@yandex.ru>
2026-03-04 10:44:34 +0300

    task 4 and result files

--- Task4/main.py
+++ Task4/main.py
@@ -1,8 +1,31 @@
 import argparse
+import os
+import tf_idf_calculator as calc
+
+
+def write_statistics_for_documents_to_files(tf, idf, output_dir):
+    os.makedirs(output_dir, exist_ok=True)
+
+    for doc, terms_tf in tf.items():
+        output_path = os.path.join(output_dir, doc)
+        with open(output_path, "w", encoding="utf-8") as f:
+            for term, tf_value in terms_tf.items():
+                idf_value = idf.get(term, 0.0)
+                tf_idf = tf_value * idf_value
+                f.write(f"{term} {idf_value} {tf_idf}\n")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Calculate tf-idf for tokens and lemmas")
 
-    parser.add_argument("--input_dir", default="../Task2/Task2Results")
+    parser.add_argument("--input_dir", default="Task2/Task2Results")
+    parser.add_argument("--tokens_output_dir", default="Task4/Task4Results/tokens")
+    parser.add_argument("--lemmas_output_dir", default="Task4/Task4Results/lemmas")
 
     args = parser.parse_args()
+
+    tokens_tf, token_idf = calc.calculate_tf_and_idf_for_tokens(args.input_dir)
+    lemmas_tf, lemmas_idf = calc.calculate_tf_and_idf_for_lemmas(args.input_dir)
+
+    write_statistics_for_documents_to_files(tokens_tf, token_idf, args.tokens_output_dir)
+    write_statistics_for_documents_to_files(lemmas_tf, lemmas_idf, args.lemmas_output_dir)
---
+++ Task4/tf_idf_calculator.py
@@ -0,0 +1,120 @@
+import math
+import os
+from collections import Counter
+from collections import defaultdict
+
+TOKEN_FILENAME_SUFFIX = '_токены.txt'
+LEMMA_FILENAME_SUFFIX = '_леммы.txt'
+
+
+def load_token_documents(folder_path):
+    documents = {}
+
+    if not os.path.exists(folder_path):
+        raise FileNotFoundError(folder_path)
+
+    listed_files = os.listdir(folder_path)
+    listed_files.remove("index_токены.txt")
+
+    token_files = filter(lambda x: x.endswith(TOKEN_FILENAME_SUFFIX), listed_files)
+    token_files = list(token_files)
+    for filename in token_files:
+        path = os.path.join(folder_path, filename)
+
+        with open(path, encoding="utf-8") as f:
+            tokens = f.read().lower().split()
+
+        documents[filename] = tokens
+
+    return documents
+
+
+def load_lemma_documents(folder_path):
+    documents = {}
+
+    if not os.path.exists(folder_path):
+        raise FileNotFoundError(folder_path)
+
+    listed_files = os.listdir(folder_path)
+    listed_files.remove("index_леммы.txt")
+
+    token_files = filter(lambda x: x.endswith(LEMMA_FILENAME_SUFFIX), listed_files)
+    token_files = list(token_files)
+    for filename in token_files:
+        path = os.path.join(folder_path, filename)
+        lemmas = []
+
+        with open(path, encoding="utf-8") as f:
+            for line in f:
+                line = line.strip().lower()
+
+                if not line:
+                    continue
+
+                lemma = line.split()[0]
+                lemmas.append(lemma)
+
+        documents[filename] = lemmas
+
+    return documents
+
+
+def compute_tf(documents):
+    tf = {}
+
+    for doc, tokens in documents.items():
+        total_terms = len(tokens)
+        counts = Counter(tokens)
+
+        tf[doc] = {
+            term: count / total_terms
+            for term, count in counts.items()
+        }
+
+    return tf
+
+
+def compute_idf(documents):
+    df = defaultdict(int)
+
+    for tokens in documents.values():
+        unique_terms = set(tokens)
+        for term in unique_terms:
+            df[term] += 1
+
+    n = len(documents)
+
+    idf = {
+        term: math.log(n / freq)
+        for term, freq in df.items()
+    }
+
+    return idf
+
+
+def compute_tfidf(tf, idf):
+    tfidf = {}
+
+    for doc, terms in tf.items():
+        tfidf[doc] = {
+            term: value * idf[term]
+            for term, value in terms.items()
+        }
+
+    return tfidf
+
+
+def calculate_tf_and_idf_for_tokens(input_dir):
+    token_documents = load_token_documents(input_dir)
+    token_tf = compute_tf(token_documents)
+    token_idf = compute_idf(token_documents)
+
+    return token_tf, token_idf
+
+
+def calculate_tf_and_idf_for_lemmas(input_dir):
+    lemma_documents = load_lemma_documents(input_dir)
+    lemma_tf = compute_tf(lemma_documents)
+    lemma_idf = compute_idf(lemma_documents)
+
+    return lemma_tf, lemma_idf
